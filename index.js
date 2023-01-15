f2dc0f0f28989bc7283909826f433f8996d7acc4
JackOliCode <jack.v.oliver@gmail.com>
2023-01-15 10:53:25 +0000

    added topMovies object and GET request for /movies
    These 6 movies are phase one of MCU. Eventually will change topMovies to phaseOne

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
-const express = require('express');
-const app = express();
+const express = require('express'),
+    morgan = require('morgan');
+
+const app = express();  
+
+let topMovies = [
+    {
+        title: 'Iron Man',
+        release_date: 'May 2, 2008'
+    }
+    {
+        title: 'The Incredible Hulk',
+        release_date: 'June 13, 2008'
+    }
+    {
+        title: 'Iron Man 2',
+        release_date: 'May 7, 2010'
+    }
+    {
+        title: 'Thor',
+        release_date: 'May 6, 2011'
+    }
+    {
+        title: 'Captain America: The First Avenger',
+        release_date: 'July 22, 2011'
+    }
+    {
+        title: 'The Avengers',
+        release_date: 'May 4, 2012'
+    }
+];
+
+// GET requests
+app.get('/movies', (req, res) => {
+    res.json(topMovies);
+});
