18d991ed8df01aee7105ed0f1cbd744bc9e6eb40
JackOliCode <jack.v.oliver@gmail.com>
2023-01-15 10:59:23 +0000

    added commas to seperate array objects

--- index.js
+++ index.js
@@ -7,23 +7,23 @@ let topMovies = [
     {
         title: 'Iron Man',
         release_date: 'May 2, 2008'
-    }
+    },
     {
         title: 'The Incredible Hulk',
         release_date: 'June 13, 2008'
-    }
+    },
     {
         title: 'Iron Man 2',
         release_date: 'May 7, 2010'
-    }
+    },
     {
         title: 'Thor',
         release_date: 'May 6, 2011'
-    }
+    },
     {
         title: 'Captain America: The First Avenger',
         release_date: 'July 22, 2011'
-    }
+    },
     {
         title: 'The Avengers',
         release_date: 'May 4, 2012'
