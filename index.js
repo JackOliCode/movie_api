e4a96b29269df9564121882fca2ad16fb6ce5ddd
JackOliCode <jack.v.oliver@gmail.com>
2023-01-21 11:26:16 +0000

    added users objects

--- index.js
+++ index.js
@@ -6,25 +6,48 @@ const app = express();
 
 app.use(bodyParser.json());
 
+// define users array
+let users = [
+    {
+        id: 1,
+        userName: 'filmlover69',
+        faveMovies: []
+    },
+    {
+        id: 2,
+        userName: 'McLovin',
+        faveMovies: ['Superbad']
+    }
+]
+
+
 let movies = [
     {
         title: 'Iron Man',
         release_date: 'May 2, 2008'
     },
     {
-        title: 'The Incredible Hulk',
+        title: 'Superbad',
         release_date: 'June 13, 2008'
     },
     {
-        title: 'Iron Man 2',
+        title: 'Scary Movie',
         release_date: 'May 7, 2010'
     },
     {
-        title: 'Thor',
-        release_date: 'May 6, 2011'
+        title: 'American Psycho',
+        release_date: 'May 6, 2011',
+        genre: {
+            name: 'Horror',
+            description: 'Insert description here'
+        },
+        director: {
+            name: 'Mary Harron',
+            bio: 'Mary Harron (born January 12, 1953) is a Canadian filmmaker and screenwriter. She gained recognition for her role in writing and directing several independent films, including I Shot Andy Warhol (1996), American Psycho (2000), and The Notorious Bettie Page (2005)'
+        }
     },
     {
-        title: 'Captain America: The First Avenger',
+        title: '',
         release_date: 'July 22, 2011'
     },
     {
