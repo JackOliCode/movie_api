9a69b0e469514c20d417176c6a07fd703542d47d
JackOliCode <jack.v.oliver@gmail.com>
2023-02-10 18:03:45 +0000

    removed in memory movies and fixed error that was stopping me from connecting to DB
    change mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true }); to mongoose.connect('mongodb://127.0.0.1:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
-const Models = require('./models/model.js')
+const Models = require('./models/model.js');
 
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://127.0.0.1:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
 const express = require('express'),
   bodyParser = require('body-parser'),
@@ -15,111 +15,61 @@ const app = express();
 // USE requests
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// define users array
-let users = [
-    {
-        id: 1,
-        name: 'filmlover69',
-        faveMovies: []
-    },
-    {
-        id: 2,
-        name: 'Hansel-so-hot-right-now',
-        faveMovies: ['Zoolander']
-    }
-]
-
-// define movies array
-
-let movies = [
-    {
-        title: 'American Psycho',
-        release_date: 'May 6, 2011',
-        genre: {
-            name: 'Horror',
-            description: 'Scary and bloody'
-        },
-        director: {
-            name: 'Mary Harron',
-            birthdate: 'January 12, 1953',
-            bio: 'Mary Harron (born January 12, 1953) is a Canadian filmmaker and screenwriter. She gained recognition for her role in writing and directing several independent films, including I Shot Andy Warhol (1996), American Psycho (2000), and The Notorious Bettie Page (2005)'
-        },
-        description: 'A wealthy New York City investment banking executive, Patrick Bateman, hides his alternate psychopathic ego from his co-workers and friends'
-    },
-    {
-        title: 'Shrek',
-        release_date: 'April 22, 2001',
-        genre: {
-            name: 'Adventure',
-            description: 'Walk around looking for stuff, usually with a princess in tow'
-        },
-        director: {
-            name: 'Andrew Adamson',
-            birthdate: 'December 1, 1966',
-            bio: 'Andrew Adamson was born on 1 December 1966 in Auckland, New Zealand. He is a producer and director, known for Shrek 2 (2004), Shrek (2001) and The Chronicles of Narnia: The Lion, the Witch and the Wardrobe (2005).'
-        },
-        description: 'A mean lord exiles fairytale creatures to the swamp of a grumpy ogre, who must go on a quest and rescue a princess for the lord in order to get his land back.'
-    },
-    {
-        title: 'The Notebook',
-        release_date: 'June 25, 2004',
-        genre: {
-            name: 'Romance',
-            description: 'Gets you right in the feels. Bring tissues.'
-        },
-        director: {
-            name: 'Nick Cassavetes',
-            birthdate: '01/01/61',
-            bio: 'Nick Cassavetes was born in New York City, the son of actress Gena Rowlands and Greek-American actor and film director John Cassavetes. As a child, he appeared in two of his father\'s films: Husbands (1970) and A Woman Under the Influence (1974).'
-        },
-        description: 'A poor yet passionate young man (Ryan Gosling) falls in love with a rich young woman (Rachel McAdams), giving her a sense of freedom, but they are soon separated because of their social differences.'
-    },
-    {
-        title: 'Zoolander',
-        release_date: 'September 28, 2001',
-        genre: {
-            name: 'Comedy',
-            description: 'It\'s all about the laughs'
-        },
-        director: {
-            name: 'Ben Stiller',
-            birthdate: 'November 30, 1965',
-            bio: 'Benjamin Edward Meara Stiller was born on November 30, 1965, in New York City, New York, to legendary comedians Jerry Stiller and Anne Meara. His father was of Austrian Jewish and Polish Jewish descent, and his mother was of Irish Catholic descent (she converted to Judaism).'
-        },
-        description: 'At the end of his career, a clueless fashion model is brainwashed to kill the Prime Minister of Malaysia.'
-    },
-    {
-        title: 'The Lord of the Rings: The Return of the King',
-        release_date: 'September 28, 2001',
-        genre: {
-            name: 'Action',
-            description: 'I\'m going on an Adventure - Bilbo Baggins'
-        },
-        director: {
-            name: 'Peter Jackson',
-            birthdate: 'October 31, 1961',
-            bio: 'Sir Peter Jackson made history with The Lord of the Rings trilogy, becoming the first person to direct three major feature films simultaneously'
-        },
-        description: 'Gandalf and Aragorn lead the World of Men against Sauron\'s army to draw his gaze from Frodo and Sam as they approach Mount Doom with the One Ring.'
-    }
-];
 
 // USER orientated URL requests below ------------------- 
 
 // allow new users to register POST/ CREATE
+//Add a user
+/* We’ll expect JSON in this format
+{
+  ID: Integer,
+  Username: String,
+  Password: String,
+  Email: String,
+  Birthday: Date
+}*/
 
 app.post('/users', (req, res) => {
-    const newUser = req.body;
+    Users.findOne({ Username: req.body.Username })
+      .then((user) => {
+        if (user) {
+          return res.status(400).send(req.body.Username + 'already exists');
+        } else {
+          Users
+            .create({
+              Username: req.body.Username,
+              Password: req.body.Password,
+              Email: req.body.Email,
+              Birthday: req.body.Birthday
+            })
+            .then((user) =>{res.status(201).json(user) })
+          .catch((error) => {
+            console.error(error);
+            res.status(500).send('Error: ' + error);
+          })
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send('Error: ' + error);
+      });
+  });
 
-    if (newUser.name) {
-        newUser.id = uuid.v4();
-        users.push(newUser);
-        res.status(201).json(newUser)
-    } else {
-        res.status(400).send("users need names")
-    }
-})
+  // Get all users
+app.get('/users', (req, res) => {
+    Users.find()
+      .then((users) => {
+        res.status(201).json(users);
+      })
+      .catch((err) => {
+        console.error(err);
+        res.status(500).send('Error: ' + err);
+      });
+  });
+
+
 
 //  Allow users to update their user info (name); UPDATE // PUT
 
