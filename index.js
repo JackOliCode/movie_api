a4f8e96689007320b58d13dcfe6867d50e401bf2
JackOliCode <jack.v.oliver@gmail.com>
2023-02-20 19:02:40 +0000

    removed authentication from POST users endpoint

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ require('./passport');
   Birthday: Date
 }*/
 
-app.post('/users', passport.authenticate('jwt', {session: false}), (req, res) => {
+app.post('/users', (req, res) => {
     Users.findOne({ Username: req.body.Username })
       .then((user) => {
         if (user) {
