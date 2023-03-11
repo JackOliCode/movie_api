e51a3197be3b507712e5ac323c730d5eee17434d
JackOliCode <jack.v.oliver@gmail.com>
2023-03-11 13:47:25 +0000

    trying to specify CORS instead of leaving it open

--- index.js
+++ index.js
@@ -23,8 +23,18 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const cors = require('cors');
-app.use(cors());
-//let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://jackoc-myflix.onrender.com/'];
+//app.use(cors());
+let allowedOrigins = ['http://localhost:8080', 'http://localhost:1234', 'https://jackoc-myflix.onrender.com/', 'http://jackoc-myflix.onrender.com/login'];
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 const { check, validationResult } = require('express-validator');
 
